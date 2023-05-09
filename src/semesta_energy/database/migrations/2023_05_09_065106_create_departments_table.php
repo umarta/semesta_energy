3a386c19693e4bf2575dc50b10adb7a1cf5fce73
Rahmad Umarta <rahmadumarta@gmail.com>
2023-05-09 14:37:42 +0700

    downgrade laravel

---
+++ src/semesta_energy/database/migrations/2023_05_09_065106_create_departments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('department_name');
+            $table->string('description');
+            $table->unsignedBigInteger('head_of_department_id')->nullable();
+            $table->foreign('head_of_department_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
---
+++ src/semesta_energy/database/migrations/2023_05_09_065334_create_positions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_position_name');
+            $table->string('description');
+            $table->foreignId('department_id')->references('id')->on('departments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('positions');
+    }
+};

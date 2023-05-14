20ae24c22eca8a06535ad26e62a5f80e0bf1f664
Rahmad Umarta <rahmadumarta@gmail.com>
2023-05-14 16:18:43 +0700

    add (login|register) to route

--- src/semesta_energy/routes/api.php
+++ src/semesta_energy/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [UsersController::class, 'login'])->name('auth.login');
+    Route::post('register', [UsersController::class, 'register'])->name('auth.register');
 });
